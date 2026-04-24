e772b0344999c13621df28b3c6c8db3e37976891
Artem Kokorin <kokorin2005@mail.ru>
2026-04-24 17:30:02 +0600

    init

---
+++ src/main/java/ru/omstu/App.java
@@ -0,0 +1,13 @@
+package ru.omstu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
